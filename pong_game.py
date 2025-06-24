5812e5ca00a55a8b4ce8ec25e04ac6d95d3d46f2
Ren <renzapnta4@gmail.com>
2025-06-24 12:29:09 +0800

    setting the game's elements' parameters

--- pong_game.py
+++ pong_game.py
@@ -9,10 +9,17 @@ screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pong Game")
 
+#Game rectangles
+ball = pygame.Rect(screen_width/2 - 15,screen_height/2 - 15,30,30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70,10,140)
+opponent = pygame.Rect(10, screen_height / 2 - 70,10,140)
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quitl()
+            pygame.quit()
             sys.exit()
 
     pygame.display.flip()
