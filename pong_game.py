da9dc04f51383a384f71b40e9b4fbb79b6cc3a69
Ren <renzapnta4@gmail.com>
2025-06-24 12:38:15 +0800

    Added blocks of code (#visuals) that draws the fundamentl elements of the game on the displayed window

--- pong_game.py
+++ pong_game.py
@@ -10,12 +10,12 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pong Game")
 
 #Game rectangles
-ball = pygame.Rect(screen_width/2 - 15,screen_height/2 - 15,30,30)
-player = pygame.Rect(screen_width - 20, screen_height / 2 - 70,10,140)
-opponent = pygame.Rect(10, screen_height / 2 - 70,10,140)
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+opponent = pygame.Rect(10 , screen_height / 2 - 70, 10, 140)
 
 #Colors
-light_grey = (200,200,200)
+light_grey = (200, 200, 200)
 bg_color = pygame.Color('grey12')
 
 while True:
@@ -24,5 +24,14 @@ while True:
             pygame.quit()
             sys.exit()
 
+    #Visuals
+    screen.fill(bg_color)
+    pygame.draw.rect(screen,light_grey, player)
+    pygame.draw.rect(screen,light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0 ), (screen_width/2, screen_height))
+
+
+    #updataes the game window
     pygame.display.flip()
     clock.tick(60)
